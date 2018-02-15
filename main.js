03dd7fd726bd4b664ec00812d13fb4448c03ad7a
sczerber <binarygun90@gmail.com>
2018-02-15 19:55:47 +0200

    Was adde Random function for random speed and location of our rectangle

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 
 var currentPos = 0;
 
-function animate() {  
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');  
-  ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);  
-  ctx.fillRect(320, currentPos, 20, 20);
-  currentPos += 1;
-  if(currentPos >= canvas.clientHeight) {
-    currentPos = 0;
-  }
-  requestAnimationFrame(animate);
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+
+var x = rand(0, 640);
+var speedRec = rand(1, 20);
+
+function animate() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);
+    ctx.fillRect(x, currentPos, 20, 20);
+    currentPos += speedRec;
+    if(currentPos >= canvas.clientHeight) {
+        currentPos = 0;
+        x = rand(0, 640);
+        speedRec = rand(1, 20);
+    }
+    requestAnimationFrame(animate);
+}
 document.body.onload = animate;
