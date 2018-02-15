6edfcaa3fdac34f1419d9f869d26633ba78dd5f4
sczerber <binarygun90@gmail.com>
2018-02-15 21:10:31 +0200

    Was added function for random color

--- main.js
+++ main.js
@@ -5,20 +5,28 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function generateColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
 
-var x = rand(0, 640);
-var speedRec = rand(1, 20);
+var x = rand(-10, 620);
+var speed = rand(1, 7);
+var color = generateColor();
+
 
 function animate() {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);
+    ctx.fillStyle = color;
+    ctx.strokeStyle = color;
     ctx.fillRect(x, currentPos, 20, 20);
-    currentPos += speedRec;
+    currentPos += speed;
     if(currentPos >= canvas.clientHeight) {
         currentPos = 0;
-        x = rand(0, 640);
-        speedRec = rand(1, 20);
+        x = rand(-10, 620);
+        speed = rand(1, 7);
+        color = generateColor();
     }
     requestAnimationFrame(animate);
 }
