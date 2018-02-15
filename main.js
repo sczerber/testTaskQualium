51d109ed4c2eef6f86fc6a70b207d291e406658e
sczerber <binarygun90@gmail.com>
2018-02-15 19:45:25 +0200

    Existed files

---
+++ main.js
@@ -0,0 +1,16 @@
+
+var currentPos = 0;
+
+function animate() {  
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');  
+  ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);  
+  ctx.fillRect(320, currentPos, 20, 20);
+  currentPos += 1;
+  if(currentPos >= canvas.clientHeight) {
+    currentPos = 0;
+  }
+  requestAnimationFrame(animate);
+}
+
+document.body.onload = animate;
