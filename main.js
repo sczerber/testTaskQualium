7ff00b2c7de533901c14df31d343ee3007e28446
sczerber <binarygun90@gmail.com>
2018-02-16 05:28:05 +0200

    ForThe X-weight to check and look on my mistakes

--- main.js
+++ main.js
@@ -1,33 +1,59 @@
+// var start = document.getElementById('button__start');
+// start.onclick = function() {
+//     animate();
+// };
 
-var currentPos = 0;
+function init() {
+    var currentPos = 0;
 
-function rand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function generateColor() {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16)
-}
-
-var x = rand(-10, 620);
-var speed = rand(1, 7);
-var color = generateColor();
-
-
-function animate() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);
-    ctx.fillStyle = color;
-    ctx.strokeStyle = color;
-    ctx.fillRect(x, currentPos, 20, 20);
-    currentPos += speed;
-    if(currentPos >= canvas.clientHeight) {
-        currentPos = 0;
-        x = rand(-10, 620);
-        speed = rand(1, 7);
-        color = generateColor();
+    function getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
-    requestAnimationFrame(animate);
+
+    function generateColor() {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16)
+    }
+
+    var coordinationX = getRandomNumber(0, 620);
+    var speedOfRectangle = getRandomNumber(1, 7);
+    var colorOfRectangle = generateColor();
+    var scores = 0;
+
+    function count() {
+        scores++;
+        document.getElementById('score').innerHTML = scores;
+    }
+
+    function rectDown() {
+        currentPos = getRandomNumber(-200, 0);
+        coordinationX = getRandomNumber(0, 620);
+        speedOfRectangle = getRandomNumber(1, 7);
+        colorOfRectangle = generateColor();
+    }
+
+
+    function animate() {
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);
+        ctx.fillStyle = colorOfRectangle;
+        ctx.strokeStyle = colorOfRectangle;
+        ctx.fillRect(coordinationX, currentPos, 20, 20);
+        currentPos += speedOfRectangle;
+        if (currentPos >= canvas.clientHeight) {
+            rectDown();
+        }
+        canvas.addEventListener('mousedown', function (e) {
+            if (e.pageX > coordinationX && e.pageX < coordinationX + 40 && e.pageY > currentPos && e.pageY < currentPos + 40) {
+                count();
+                rectDown();
+            }
+        });
+
+        requestAnimationFrame(animate);
+    }
+
+    document.body.onload = animate;
+
 }
-document.body.onload = animate;
+init();
